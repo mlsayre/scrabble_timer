83d684487483db4ba4306351367cd7e5c75e9520
Matt Sayre <mlsayre@gmail.com>
2013-06-14 14:40:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrabbleTimer::Application.config.secret_token = 'd633256211ca3d43d3660940aa3f307e66d96f6995485ba0878ccaf1732b0bbb47fb428a49a92298e6b861e63757fb0de9f8bf61541ddb30680f730b83d8a067'
